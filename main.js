e712acfe49f78acdec47468047d019fb984583cb
amirakhaled2027 <155392660+amirakhaled2027@users.noreply.github.com>
2024-01-06 02:01:56 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+let names =['james', 'sara', 'kate', 'watson', 'sara', 'james'];
+
+console.log(names.indexOf('james'));
+console.log(names.indexOf('james', -1));
+console.log(names.lastIndexOf('sara'));
+console.log(names.lastIndexOf('sara', -1));
+
+console.log(names.includes('sara'));
+console.log(names.includes('amira'));
